25320ace572708c47e713022fa27ad7cd67b92be
HawraFa <hawrakhalifa2004@gmail.com>
2024-12-09 18:58:13 +0300

    Update profile.php

--- Profile/profile.php
+++ Profile/profile.php
@@ -29,22 +29,23 @@ $profilePicture = !empty($user['profile_picture']) ? $user['profile_picture'] : 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css">
-    <link rel="stylesheet" href="../config/styles.css">
+    <link rel="stylesheet" href="../Rooms/styles2.css">
     <link rel="stylesheet" href="profile.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body>
 <div class="sidebar">
     <ul class="sidebar-links">
-        <li><a href="../Rooms/home.php">HOME</a></li>
-        <li><a href="../Rooms/rooms.php">ROOMS</a></li>
-        <li><a href="../bookingSystem/booking_list.php">BOOKINGS</a></li>
-        <li><a href="../ReportingSystem/ReportingAndAnalytics.php">REPORTING</a></li>
+    <li><a href="../Rooms/home.php"><i class="fas fa-home"></i> HOME</a></li>
+    <li><a href="../Rooms/rooms.php"><i class="fas fa-door-open"></i> ROOMS</a></li>
+        <li><a href="../bookingSystem/booking_list.php"><i class="fas fa-calendar-check"></i> BOOKINGS</a></li>
+        <li><a href="../ReportingSystem/ReportingAndAnalytics.php"><i class="fas fa-chart-line"></i> REPORTING</a></li>
         <!-- Only display Admin link if user_type is admin -->
         <?php if (isset($_SESSION['user_type']) && $_SESSION['user_type'] === 'admin'): ?>
-            <li><a href="../AdminFuncs/admin.php">ADMIN</a></li>
+            <li><a href="../AdminFuncs/admin.php"><i class="fas fa-user-shield"></i> ADMIN</a></li>
         <?php endif; ?>
-        <li><a href="../LoginandRegistration/login.php">LOGOUT</a></li>
+        <li><a href="../LoginandRegistration/login.php"><i class="fas fa-sign-out-alt"></i> LOGOUT</a></li>
     </ul>
 </div>
 
