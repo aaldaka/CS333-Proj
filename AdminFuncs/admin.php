cedaf5e28d6f54d34f59c82deeae52486556709a
Alya Aldaka <alyaaldaka08@gmail.com>
2024-12-09 13:50:33 +0300

    removed a line

--- AdminFuncs/admin.php
+++ AdminFuncs/admin.php
@@ -54,17 +54,13 @@ $adminLogs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <div class="sidebar">
     <ul class="sidebar-links">
-        <li><a href="../Rooms/home.php">HOME</a></li>
+        <li><a href="../Rooms/home.php"><i class='bx bxs-home-smile'></i>HOME</a></li>
         <li><a href="../Rooms/rooms.php">ROOMS</a></li>
         <li><a href="../bookingSystem/bookings.php">BOOKINGS</a></li>
         <li><a href="../Profile/profile.php">PROFILE</a></li>
         <li><a href="../LoginandRegister/login.php">LOGOUT</a></li>
-        <?php if (isset($_SESSION['user_type']) && $_SESSION['user_type'] === 'admin'): ?>
-            <li><a href="admin.php">ADMIN</a></li>
-        <?php endif; ?>
     </ul>
 </div>
-
 
 <div class="main-content" id="mainContent">
     <button class="toggle-btn" onclick="toggleSidebar()">&#9776;</button>
