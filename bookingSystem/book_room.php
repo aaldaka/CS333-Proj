e04bf07227dd67670ff143f2a5f7f944c3801da6
KhaireyaAlhayki <khayria.alhayki39@gmail.com>
2024-12-09 18:07:33 +0300

    Update book_room.php

--- bookingSystem/book_room.php
+++ bookingSystem/book_room.php
@@ -6,7 +6,7 @@ include 'booking_functions.php';  // Include helper functions
 
 // Check if the user is logged in
 if (!isset($_SESSION['user_id'])) {
-    header('Location: login.php');
+    header('Location: ../config/login.php');
     exit();
 }
 
