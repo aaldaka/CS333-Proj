391692901d8a418d87845d24644d56af4522efae
Maryam <160751317+meemeeah@users.noreply.github.com>
2024-12-09 19:49:18 +0300

    Delete Rooms/script.js

--- Rooms/script.js
+++
@@ -1,7 +0,0 @@
-function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const mainContent = document.querySelector('.main-content');
-    sidebar.classList.toggle('open');
-     mainContent.classList.toggle('shifted');
-}
-
